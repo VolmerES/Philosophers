e3a552f23c3fed0028ba017e21aa5bd720eb6097
Jdelorme <volmer394@gmail.com>
2024-10-24 20:11:35 +0200

    add: initialitation of mutex safe

--- init.c
+++ init.c
@@ -6,51 +6,48 @@
 /*   By: jdelorme <jdelorme@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 14:23:15 by jdelorme          #+#    #+#             */
-/*   Updated: 2024/10/24 17:07:48 by jdelorme         ###   ########.fr       */
+/*   Updated: 2024/10/24 18:59:19 by jdelorme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	ft_mutex_safe(t_mtx *mutex, t_code code)
+static void	ft_assign_forks(t_philo *philo, t_fork *forks, int pos)
 {
-	if (code == INIT)
-		ft_error_mutex(code, pthread_mutex_init(mutex, NULL));
-	else if (code == UNLOCK)
-		ft_error_mutex(code, pthread_mutex_unlock(mutex));
-	else if (code == LOCK)
-		ft_error_mutex(code, pthread_mutex_lock(mutex));
-	else if (code == DESTROY)
-		ft_error_mutex(code, pthread_mutex_destroy(mutex));
-	else
-		ft_error_exit(RED "Error: Mutex operation not found\n" RESET);
+
 }
 
-void	*ft_malloc_safe(size_t size)
+static void	ft_philosphers_init(t_table *table)
 {
-	void	*ptr;
-	
-	ptr = malloc(size);
-	if (!ptr)
-		ft_error_exit(RED "Error: Malloc failed\n" RESET);
-	printf("Malloc success\n");
-	return (ptr);
-}
+	int	i;
+	t_philo	*philo;
 
-void	ft_thread_safe(pthread_t *thread, t_code code, void *(*func)(void *), void *arg)
-{
-	if (code == CREATE)
-		ft_error_thread(code, pthread_create(thread, NULL, func, arg));
-	else if (code == JOIN)
-		ft_error_thread(code, pthread_join(*thread, NULL));
-	else if (code == DETACH)
-		ft_error_thread(code, pthread_detach(*thread));
-	else
-		ft_error_exit(RED "Error: Thread operation not found\n" RESET);
+	i = -1;
+	while (++i < table->philo_nbr)
+	{
+		philo = table->philos + i;
+		//? Esto hace que en compracion con los forks, los
+		//? filos, tengan un numero mal al empezar por 1, CAMBIAR??
+		philo->philo_id = i + 1;
+		philo->full = -1;
+		philo->meal_count = 0;
+		philo->table = table;
+		ft_assign_forks(philo, table->forks, i);
+	}
 }
 
 void	ft_init_data(t_table *table)
 {
+	int	i;
+
+	i = -1;
 	table->end_simulation = 0;
 	table->philos = ft_malloc_safe(sizeof(t_philo) * table->philo_nbr);
+	table->forks = ft_malloc_safe(sizeof(t_fork) * table->philo_nbr);
+	while(++i < table->philo_nbr)
+	{
+		ft_mutex_safe(&table->forks[i].fork, INIT);
+		table->forks[i].fork_id = i;
+	}
+	ft_philosophers_init(table);
 }
